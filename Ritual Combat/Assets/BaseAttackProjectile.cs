d5eec27f26ff2820bbd19c96123b8fe0cfef1c00
nimanym <nimanym@gmail.com>
2016-01-30 15:25:49 +0100

    no message

---
+++ Ritual Combat/Assets/BaseAttackProjectile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseAttackProjectile : MonoBehaviour {
+
+    public int damage = 5;
+    public GameObject projectile;
+    public float attackSpeed = 1.0f;
+    public int favourOnHit = 10;
+    bool attacking;
+    float cooldown;
+    GameObject shot;
+
+    // Use this for initialization
+    void Start()
+    {
+        cooldown = 1 / attackSpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        cooldown -= Time.deltaTime;
+
+        if (Input.GetAxis("BaseAttack" + GetComponentInParent<CharacterMovement>().player.ToString()) > 0)
+        {
+            attacking = true;
+        }
+        else {
+            attacking = false;
+        }
+        
+        if(attacking && cooldown <= 0)
+        {
+            shot=Instantiate(projectile);
+            cooldown = cooldown = 1 / attackSpeed;
+        }
+        
+    }
+}
