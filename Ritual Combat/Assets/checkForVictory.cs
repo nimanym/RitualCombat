3c6abef1b132ca96eea16c2f2bb612c67314e3bb
nimanym <nimanym@gmail.com>
2016-01-31 16:42:22 +0100

    FINAL

--- Ritual Combat/Assets/checkForVictory.cs
+++ Ritual Combat/Assets/checkForVictory.cs
@@ -4,14 +4,21 @@ using System.Collections;
 public class checkForVictory : MonoBehaviour {
 
     GameObject[] players;
+    float timer;
+    bool finished;
 
 	// Use this for initialization
 	void Start () {
+        finished = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        timer -= Time.deltaTime;
+        if (finished && timer <= 0)
+        {
+            Application.LoadLevel("MainMenu");
+        }
     }
 
     void OnGUI()
@@ -24,6 +31,10 @@ public class checkForVictory : MonoBehaviour {
             string texto = "Player " + players[0].GetComponent<CharacterMovement>().player.ToString() + " Wins";
             style.fontSize = 40;
             GUI.Label(new Rect((Screen.width/2)-100, (Screen.height / 2)-20, 100, 20), texto, style);
+            if (!finished) { 
+                timer = 5.0f;
+                finished = true;
+            }
         }
     }
 }
